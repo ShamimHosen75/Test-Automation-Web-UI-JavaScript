0867e6013f6a5a00ee7d7c3d1927e19476c65a0b
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-04-07 21:18:22 +0600

    first commit

---
+++ TAU-Intro-to-JS/04.function.js
@@ -0,0 +1,17 @@
+function sum (a, b){
+  return a+b;
+}
+
+console.log(sum(20, 10)); //30
+
+
+// Another examples 
+function product (a, b){
+  return a*b;
+}
+
+console.log(product(5,15)); //75
+
+// Another Examples 
+const bigProduct = product(5353, 134534);
+console.log(bigProduct);
