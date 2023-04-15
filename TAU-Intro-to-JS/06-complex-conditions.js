565f2e217b3b4f081d8fdbf108ed29013aeb6aef
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-04-16 01:39:56 +0600

    first commit

--- TAU-Intro-to-JS/06-complex-conditions.js
+++ TAU-Intro-to-JS/06-complex-conditions.js
@@ -8,5 +8,5 @@ if (car.price < 2000 || (car.color === "red" &&
   car.numDoors === 4)) {
   console.log("We'll take it!");
 } else {
-  console.log("help, it's best to keep looking");
+  console.log("Help, it's best to keep looking!!")
 }
